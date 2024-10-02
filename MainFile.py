29dc3ec0df57d3e1782c72a13038df7066b6e318
eduardoSanchezLil <149595580+eduardoSanchezLil@users.noreply.github.com>
2024-10-02 13:24:07 +0000

    zonas

--- MainFile.py
+++ MainFile.py
@@ -6,3 +6,10 @@
 #Nombre del robot, tiempo en minutos para limpiar un metro cuadrado 
 parametros = ("robot_aspirador",2) 
 
+# Empleo de diccionnarios para crear las zonas  
+zona1={"largo":500,"ancho":150}  
+zona2={"largo":309,"ancho":480}  
+zona3={"largo":480,"ancho":101}  
+zona4={"largo":90,"ancho":220} 
+
+
