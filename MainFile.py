c4483d4290c2a79aa0dc86ce1bb3ac1564f963d6
eduardoSanchezLil <149595580+eduardoSanchezLil@users.noreply.github.com>
2024-10-02 13:21:16 +0000

    segundas lineas

--- MainFile.py
+++ MainFile.py
@@ -1,3 +1,8 @@
 #----------------------------------------------  
 # APLICACION  
-#---------------------------------------------- 
+#----------------------------------------------  
+  
+#Empleo de una tupla para la configuración de la aplicación  
+#Nombre del robot, tiempo en minutos para limpiar un metro cuadrado 
+parametros = ("robot_aspirador",2) 
+
